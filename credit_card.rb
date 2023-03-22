8e769470287a703dae307d918156fa7fc44992c8
jerryho <jerryjp3914@gmail.com>
2023-03-23 00:07:59 +0800

    Implement hashing method

--- credit_card.rb
+++ credit_card.rb
@@ -1,47 +1,42 @@
 require_relative './luhn_validator'
 require 'json'
+require 'rbnacl'
 
 class CreditCard
-  # TODO: mixin the LuhnValidator using an 'include' statement
-
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above
-     @number = number
+    @number = number
     @expiration_date = expiration_date
     @owner = owner
     @credit_network = credit_network
   end
 
   # returns json string
-  def to_json(*_args)
+  def to_json
     {
-      'number' => @number,
-      'expiration_date' => @expiration_date,
-      'owner' => @owner,
-      'credit_network' => @credit_network
-      # TODO: setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
   # returns all card information as single string
   def to_s
-    to_json
+    JSON.generate(to_json)
   end
 
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
-    # TODO: deserializing a CreditCard object
+    data = JSON.parse(card_s)
+    CreditCard.new(data['number'], data['expiration_date'], data['owner'], data['credit_network'])
   end
 
   # return a hash of the serialized credit card object
   def hash
-    # TODO: implement this method
-    #   - Produce a hash (using default hash method) of the credit card's
-    #     serialized contents.
-    #   - Credit cards with identical information should produce the same hash
+    to_json.hash
   end
 
   # return a cryptographically secure hash
@@ -49,5 +44,7 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    message = to_s
+    RbNaCl::Hash.sha256(message)
   end
 end
